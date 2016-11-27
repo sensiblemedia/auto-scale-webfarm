a1bf013eb6d3ab6487affdec42c557c4287f9b67
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 19:50:43 +0000

    Deploy f9c8b30
    diff --git a/prod/deploy.sh b/prod/deploy.sh
    index 552e3ac..94e2d18 100755
    --- a/prod/deploy.sh
    +++ b/prod/deploy.sh
    @@ -10,13 +10,13 @@ githash="${GIT_COMMIT_LATEST_HASH}"
    
     echo "hash=$githash"
     #touch ./doc/$hash
    -exec 3<> /doc/${GIT_COMMIT_LATEST_HASH}
    +
     echo "docker-machine create \
         --driver ${DO_DRIVE} \
         --digitalocean-access-token=${DO_TOKEN} \
         --digitalocean-size=${DO_SIZE} \
         --digitalocean-region=${DO_REGION} \
    -    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" >&3
    +    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" > /doc/host.info
    
     exec 3>&-

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -10,13 +10,13 @@ githash="${GIT_COMMIT_LATEST_HASH}"
 
 echo "hash=$githash"
 #touch ./doc/$hash
-
+exec 3<>/doc/host.info
 echo "docker-machine create \
     --driver ${DO_DRIVE} \
     --digitalocean-access-token=${DO_TOKEN} \
     --digitalocean-size=${DO_SIZE} \
     --digitalocean-region=${DO_REGION} \
-    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" > /doc/host.info
+    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"  >&3
 
 exec 3>&-
 
