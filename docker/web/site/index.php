00be1ff0b3222b7eb2fd3efc19912ea2f0afac9b
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 22:25:58 +0000

    clean up

--- docker/web/site/index.php
+++ docker/web/site/index.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'class.user.php';
 $user_login = new USER();
 //echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
-//echo 'hello the great big onion';
+echo 'hello the great big onion';
 if($user_login->is_logged_in()!="")
 {
 	$user_login->redirect('home.php');
--- prod/deploy.sh
+++ prod/deploy.sh
@@ -9,16 +9,21 @@ echo "${DO_REGION}"
 echo "GITHASH = ${GIT_COMMIT_LATEST_HASH}"
  
 
-exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
-    --digitalocean-access-token=${DO_TOKEN} \
-    --digitalocean-size=${DO_SIZE} \
-    --digitalocean-region=${DO_REGION} \
-    "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
+# exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
+    # --digitalocean-access-token=${DO_TOKEN} \
+    # --digitalocean-size=${DO_SIZE} \
+    # --digitalocean-region=${DO_REGION} \
+    # "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
 
-/usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
+echo "Setting Docker-Machine Enviroment"
+ /usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
 eval $(/usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}")
-docker-compose -f docker-compose-prod.yml build
-docker-compose up -d
+echo "Copying over deplyment"
+exec docker-machine scp -r ./docker "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}":$PWD
+echo "Executing Docker-Compose Production Build"
+exec docker-compose -f docker-compose-prod.yml build
+echo "Bringing up Docker containers on ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
+exec docker-compose up -d
 
 
 
