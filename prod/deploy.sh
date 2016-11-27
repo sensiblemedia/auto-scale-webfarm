f264722a7625ca5e8fb6ab4116e4fd7ca3aec609
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 20:53:18 +0000

    Deploy 35fa580
    diff --git a/prod/deploy.sh b/prod/deploy.sh
    index 2cb52fa..a1e5ca2 100755
    --- a/prod/deploy.sh
    +++ b/prod/deploy.sh
    @@ -2,17 +2,14 @@
    
     echo "######################## IN DEPLOYMENT ########################"
     source ./prod/digitalocean.env
    -
    +export GIT_COMMIT_LATEST_HASH=$(git log -n 1 | head -n 1 | sed -e 's/^commit //' | head -c 8)
     echo "${DO_TOKEN}"
     echo "${DO_SIZE}"
    +echo "${DO_REGION}"
     echo "GITHASH = ${GIT_COMMIT_LATEST_HASH}"
    -githash="${GIT_COMMIT_LATEST_HASH}"
    +
    
    -echo "hash=$githash"
    -#touch ./doc/$hash
    -#exec 3<>./doc/host.info
    -exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} --digitalocean-access-token=${DO_TOKEN} --digitalocean-size=${DO_SIZE} --digitalocean-region=${DO_REGION} ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}
    +exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} --digitalocean-access-token=${DO_TOKEN} --digitalocean-size=${DO_SIZE} --digitalocean-region=${DO_REGION} "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
    
    -#exec 3>&-
    
    diff --git a/prod/digitalocean.env b/prod/digitalocean.env
    index 7d86230..9557dc6 100644
    --- a/prod/digitalocean.env
    +++ b/prod/digitalocean.env
    @@ -1,12 +1,9 @@
     export DO_DRIVE=digitalocean
     export DO_TOKEN=6766bb8582b1d78b959d16344a19bdca3efbc6740b36992d425e48605e44b7ad
     export DO_SIZE=512mb
    -export DO_REGION="lon"
    +export DO_REGION="lon1"
     export DO_INSTANCE="prod"
    
    -
    -DO_DRIVE="digitalocean"
    -DO_TOKEN="6766bb8582b1d78b959d16344a19bdca3efbc6740b36992d425e48605e44b7ad"
    -DO_SIZE="512mb"
    -DO_REGION="lon"
    -DO_INSTANCE="prod"
    \ No newline at end of file
    +# export DIGITALOCEAN_ACCESS_TOKEN=6766bb8582b1d78b959d16344a19bdca3efbc6740b36992d425e48605e44b7ad
    +# export DIGITALOCEAN_SIZE="512mb"
    +# export DIGITALOCEAN_REGION="lon1"

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -9,7 +9,11 @@ echo "${DO_REGION}"
 echo "GITHASH = ${GIT_COMMIT_LATEST_HASH}"
  
 
-exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} --digitalocean-access-token=${DO_TOKEN} --digitalocean-size=${DO_SIZE} --digitalocean-region=${DO_REGION} "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
+exec /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
+    --digitalocean-access-token=${DO_TOKEN} \
+    --digitalocean-size=${DO_SIZE} \
+    --digitalocean-region=${DO_REGION} \
+    "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
 
 
 
