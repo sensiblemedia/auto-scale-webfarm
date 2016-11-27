0e5d9c07b98a65f6aa88dbc0d945ef658f37dc12
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 17:12:37 +0000

    Deploy f4ab9b9
    diff --cc auditi.json
    index 0000000,0000000..a64c006
    new file mode 100644
    --- /dev/null
    +++ b/auditi.json
    @@@ -1,0 -1,0 +1,38 @@@
    ++{
    ++  "title": null,
    ++  "version": 1,
    ++  "env": {
    ++    "TERM": "xterm-256color",
    ++    "SHELL": "/bin/bash"
    ++  },
    ++  "duration": 2.868145,
    ++  "command": null,
    ++  "width": 249,
    ++  "stdout": [
    ++    [
    ++      0.009188,
    ++      "\u001b[?1034hbash-3.2$ "
    ++    ],
    ++    [
    ++      1.895072,
    ++      "e"
    ++    ],
    ++    [
    ++      0.191497,
    ++      "x"
    ++    ],
    ++    [
    ++      0.160133,
    ++      "i"
    ++    ],
    ++    [
    ++      0.191992,
    ++      "t"
    ++    ],
    ++    [
    ++      0.415715,
    ++      "\r\n"
    ++    ]
    ++  ],
    ++  "height": 41
    ++}

---
+++ prod/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source digitalocean.env
+
+echo "${DO_TOKEN}"
+echo "${DO_SIZE}"
+echo "Docker-machine create \
+    --driver ${DO_DRIVE} \
+    --digitalocean-access-token=${DO_TOKEN} \
+    --digitalocean-size=${DO_SIZE} \
+    --digitalocean-region=${DO_REGION} \
+    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" >> doc_${GIT_COMMIT_LATEST_HASH}.out
+
---
+++ test/tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting Tests"
+echo "Current GIT HASH = ${GIT_COMMIT_LATEST_HASH}"
+
+#exit 1
+exit 0
