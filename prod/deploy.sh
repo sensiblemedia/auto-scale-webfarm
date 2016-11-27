7f40739baedfadceecb6c6adf8dc9826f7019ac2
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 23:11:03 +0000

    Deploy d92f142
    diff --git a/prod/deploy.sh b/prod/deploy.sh
    index a9c7eee..4ba3ae6 100755
    --- a/prod/deploy.sh
    +++ b/prod/deploy.sh
    @@ -9,17 +9,17 @@ echo "${DO_REGION}"
     echo "GITHASH = ${GIT_COMMIT_LATEST_HASH}"
    
    -/usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
    -     --digitalocean-access-token=${DO_TOKEN} \
    -     --digitalocean-size=${DO_SIZE} \
    -     --digitalocean-region=${DO_REGION} \
    -     "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
    +# /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
    +#      --digitalocean-access-token=${DO_TOKEN} \
    +#      --digitalocean-size=${DO_SIZE} \
    +#      --digitalocean-region=${DO_REGION} \
    +#      "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
    
     echo "Setting Docker-Machine Enviroment"
     /usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
     eval $(/usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}")
     echo "Copying over deplyment"
    -docker-machine scp -r ./docker "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}":$PWD
    +docker-machine scp -r /Users/sg/Documents/docker/auto-scale-webfarm/docker "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}":$PWD
     echo "Executing Docker-Compose Production Build"
     docker-compose -f docker-compose-prod.yml build
     echo "Bringing up Docker containers on ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
    diff --git a/run.sh b/run.sh
    new file mode 100755
    index 0000000..92af510
    --- /dev/null
    +++ b/run.sh
    @@ -0,0 +1,3 @@
    +#!/bin/bash
    +
    +./prod/deploy.sh
    \ No newline at end of file

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -9,15 +9,16 @@ echo "${DO_REGION}"
 echo "GITHASH = ${GIT_COMMIT_LATEST_HASH}"
  
 
-# /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
-#      --digitalocean-access-token=${DO_TOKEN} \
-#      --digitalocean-size=${DO_SIZE} \
-#      --digitalocean-region=${DO_REGION} \
-#      "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
+ /usr/local/bin/docker-machine create --driver ${DO_DRIVE} \
+      --digitalocean-access-token=${DO_TOKEN} \
+      --digitalocean-size=${DO_SIZE} \
+      --digitalocean-region=${DO_REGION} \
+      "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" 
 
 echo "Setting Docker-Machine Enviroment"
 /usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}"
 eval $(/usr/local/bin/docker-machine env "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}")
+sleep 4
 echo "Copying over deplyment"
 docker-machine scp -r /Users/sg/Documents/docker/auto-scale-webfarm/docker "${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}":$PWD
 echo "Executing Docker-Compose Production Build"
