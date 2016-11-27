4f388050c9264a368253b3e3c43e742d6af1d673
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2016-11-27 17:24:46 +0000

    Deploy 84fc191
    diff --git a/out b/out
    new file mode 100644
    index 0000000..e69de29
    diff --git a/prod/deploy.sh b/prod/deploy.sh
    index 5dd9132..76dbe91 100755
    --- a/prod/deploy.sh
    +++ b/prod/deploy.sh
    @@ -1,6 +1,6 @@
     #!/bin/bash
    
    -source digitalocean.env
    +source /prod/digitalocean.env
    
     echo "${DO_TOKEN}"
     echo "${DO_SIZE}"

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /prod/digitalocean.env
+source ./digitalocean.env
 
 echo "${DO_TOKEN}"
 echo "${DO_SIZE}"
@@ -9,5 +9,5 @@ echo "Docker-machine create \
     --digitalocean-access-token=${DO_TOKEN} \
     --digitalocean-size=${DO_SIZE} \
     --digitalocean-region=${DO_REGION} \
-    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" >> doc_${GIT_COMMIT_LATEST_HASH}.out
+    ${DO_INSTANCE}-${DO_REGION}-${DO_SIZE}-${GIT_COMMIT_LATEST_HASH}" #>> doc_${GIT_COMMIT_LATEST_HASH}.out
 
